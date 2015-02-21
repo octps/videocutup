0563b87993d0f4ddd59f7aebcc44899ae4f8cf1c
octps <renmo9@gmail.com>
2015-02-21 23:04:00 +0900

    swift project追加

---
+++ videocutup/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  videocutup
+//
+//  Created by kitajima on 2015/02/21.
+//  Copyright (c) 2015年 kitajima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("test")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
