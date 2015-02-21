dac20abdf26e45b69a01ba05b105cd0a93c675f5
octps <renmo9@gmail.com>
2015-02-22 02:45:07 +0900

    #22 動画の表示・再生を実装 swift avAsset

--- videocutup/ViewController.swift
+++ videocutup/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
+import CoreMedia
 
 class ViewController: UIViewController {
+    var playerItem : AVPlayerItem!
+    var videoPlayer : AVPlayer!
+
 
     override func viewDidLoad() {
+        let path = NSBundle.mainBundle().pathForResource("test", ofType: "mp4")
+        let fileURL = NSURL(fileURLWithPath: path!)
+        let avAsset = AVURLAsset(URL: fileURL, options: nil)
+
+        playerItem = AVPlayerItem(asset: avAsset)
+        videoPlayer = AVPlayer(playerItem: playerItem)
+        let videoPlayerView = AVPlayerView(frame: self.view.bounds)
+        
+        let layer = videoPlayerView.layer as AVPlayerLayer
+        layer.videoGravity = AVLayerVideoGravityResizeAspect
+        layer.player = videoPlayer
+        
+        self.view.layer.addSublayer(layer)
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func playMovie(sender: UIButton) {
+        videoPlayer.seekToTime(CMTimeMakeWithSeconds(0, Int32(NSEC_PER_SEC)))
+        videoPlayer.play()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
 }
 
+
+// レイヤーをAVPlayerLayerにする為のラッパークラス.
+class AVPlayerView : UIView{
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override class func layerClass() -> AnyClass{
+        return AVPlayerLayer.self
+    }
+    
+}
