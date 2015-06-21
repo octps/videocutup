0511fd559f9bb814cbaeb1768dac26b5db7f9274
octps <renmo9@gmail.com>
2015-06-21 15:49:03 +0900

    #24 restart for xcode7

---
+++ video/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  video
+//
+//  Created by n001 on 2015/06/19.
+//  Copyright © 2015年 n001. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import CoreMedia
+import ImageIO
+import MobileCoreServices
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    var playerItem : AVPlayerItem!
+    var videoPlayer : AVPlayer!
+    @IBOutlet weak var webView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadAddressURL()
+    }
+
+    
+    @IBAction func selectMovie(sender: AnyObject) {
+        if !UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            UIAlertView(title: "警告", message: "Photoライブラリにアクセス出来ません", delegate: nil, cancelButtonTitle: "OK").show()
+        } else {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            imagePickerController.mediaTypes = NSArray(object: kUTTypeMovie) as! [String]
+            imagePickerController.allowsEditing = false
+            imagePickerController.delegate = self
+            self.presentViewController(imagePickerController,animated:true ,completion:nil)
+        }
+        //todo momentを選択すると落ちる
+        
+    }
+
+    // カメラロールから選択後、選択した動画のurlを動画表示メソッドに渡す
+    func imagePickerController(picker: UIImagePickerController!, _didFinishPickingMediaWithInfo info: NSDictionary!) {
+        let url = info[UIImagePickerControllerMediaURL] as! NSURL!
+//        var pickedURL:NSURL = info[UIImagePickerControllerReferenceURL] as! NSURL
+        self.dismissViewControllerAnimated(true, completion: nil)
+        let JSScript =  "callFromSwift('" + url.absoluteString + "')"
+        print(JSScript)
+        webView.stringByEvaluatingJavaScriptFromString(JSScript);
+    }
+
+    func loadAddressURL() {
+        let targetURL = NSBundle.mainBundle().pathForResource("htdocs/index", ofType: "html");
+
+        let requestURL = NSURL(string: targetURL!)
+        let req = NSURLRequest(URL: requestURL!)
+        self.webView.allowsInlineMediaPlayback = true;
+        webView.loadRequest(req)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
---
+++ video/htdocs/js/video.js
@@ -0,0 +1,212 @@
+/*
+ ** 動画の長さ取得
+ */
+$(window).load(function () {
+  window.duration = window.video.duration;
+});
+
+
+/*
+ ** 初期設定
+ */
+$(function(){
+  window.duration = 0;
+  window.sequence = 0;
+  window.clipCount = 0;
+  window.clipPlayCount = 0;
+  window.clipRecord = [];
+  window.clipRecords = [];
+  window.clipTapTime = [
+    [],
+    [],
+    []
+  ];
+  window.lastClipTapTime = "";
+  window.clipPlayEndFlag = "first";
+  window.clipInterval = [];
+
+  setInterval(
+    function () {
+      window.sequence = window.sequence + 0.01; 
+
+      if (window.sequence > window.duration) {
+        window.sequence = 0;
+      }
+    }
+  , 10);
+});
+
+
+/*
+ ** クリップ登録
+ */
+$(function(){
+  $("#clip-record button").bind('touchstart'
+    , function () {
+      if (window.clipCount < 3) {
+        clipRecord.push(window.video.currentTime);
+        $(this).html("now recording...");
+      }
+      else {
+        return false;
+      }
+    }
+  ).bind('touchend'
+    , function () {
+      if (window.clipCount < 3) {
+        clipRecord.push(window.video.currentTime);
+
+        clipRecords[clipCount] = clipRecord;
+        clipRecord = [];
+
+        captureImage(window.clipCount);
+          
+        $(this).html("record");
+
+        window.clipCount = window.clipCount + 1;
+
+        if (window.clipCount > 0) {
+            btnView(window.clipCount);
+        }
+      }
+
+      else {
+          return false;
+      }
+    }
+  );
+
+  var btnView = function (index) {
+    $("#clip-play button").eq(index - 1).html('play!');
+  }
+});
+
+
+/*
+ ** クリップ登録時のイメージ取得
+ */
+var captureImage = function (index) {
+  var video  = document.getElementById('video')
+    , output = $('.capture-output div')
+    , canvas = capture(video)
+    ;
+  
+  output.eq(index).html(canvas);
+  
+  function capture(video) {
+    var w = video.videoWidth * 0.2
+    , h = video.videoHeight * 0.2
+    , canvas = document.createElement('canvas')
+    ;
+    
+    canvas.width  = w;
+    canvas.height = h;
+
+    var ctx = canvas.getContext('2d');
+    
+    ctx.drawImage(video, 0, 0, w, h);
+    
+    return canvas;
+  }
+
+  $('.capture-output').eq(index).children('.capture-time')
+    .html(
+      clipRecords[index][0].toFixed(1)
+      + ' 〜 '
+      + clipRecords[index][1].toFixed(1)
+    );
+};
+
+
+/*
+ ** クリップの再生
+ */
+var clipPlay = function (t) {
+  var index = $("#clip-play button").index(t);
+  
+  clearInterval(window.clipInterval);
+  
+  window.clipPlayCount = window.clipPlayCount + 1;
+  
+  if (clipPlayEndFlag == "first") {
+    clipTapTime[index].push(window.sequence);
+  }
+  
+  else if (clipPlayEndFlag === "false") {
+    return false;
+    //clipTapTime[index].push(window.lastClipTapTime);
+  }
+  
+  else if (clipPlayEndFlag === "true") {
+    clipTapTime[index].push(window.sequence);
+  }
+  
+  window.clipPlayEndFlag = "false";
+  window.video.currentTime = clipRecords[index][0];
+  
+  window.lastClipTapTime = clipTapTime[index][clipTapTime[index].length - 1];
+
+  window.duration = window.duration + (clipRecords[index][1] - clipRecords[index][0]);
+  
+  clipPlayToEnd(index);
+};
+
+var clipPlayToEnd = function (num) {
+  window.clipInterval = setInterval(
+    function () {
+      if (window.video.currentTime > clipRecords[num][1]) {
+        window.clipPlayEndFlag = "true";
+        window.video.currentTime = lastClipTapTime;
+        clearInterval(window.clipInterval);
+      }
+    }, 1
+  );
+}
+
+
+/*
+ ** クリップの削除
+ */
+var clipDelete = function (t) {
+  var division = $("#clip-play .box")
+    , index = $("#clip-play a").index(t)
+    , output = $('.capture-output div')
+    ;
+
+  output.eq(index).html('');
+  $('.capture-output').eq(index).children('.capture-time').html('');
+  $("#clip-play button").eq(index).html('no clip');
+  $("#clip-play").append(division.eq(index).clone());
+
+  division.eq(index).remove();
+
+  clipRecords.splice(index, 1);
+
+  window.clipCount = window.clipCount - 1;
+};
+
+
+/*
+ ** 登録クリップの再生
+ */
+$(function(){
+  setInterval(
+    function () {
+      if ($.inArray(window.sequence, clipTapTime[0]) !== -1) {
+        window.video.currentTime = clipRecords[0][0];
+        clipPlayToEnd(0);
+      }
+
+      if ($.inArray(window.sequence, clipTapTime[1]) !== -1) {
+        window.video.currentTime = clipRecords[1][0];
+        clipPlayToEnd(1);
+      }
+
+      if ($.inArray(window.sequence, clipTapTime[2]) !== -1) {
+        window.video.currentTime = clipRecords[2][0];
+        clipPlayToEnd(2);
+      }
+    }
+  , 1);
+});
+
